fc7013c797df6c8237673dabcbf5cde88edca33a
liuchengguo <liuchengguo>
2020-01-08 14:08:50 +0800

    头像

--- resources/views/admin/member/_form.blade.php
+++ resources/views/admin/member/_form.blade.php
@@ -26,23 +26,23 @@
     </div>
 </div>
 
-<div class="layui-form-item">
-    <label for="" class="layui-form-label">头像</label>
-    <div class="layui-input-block">
-        <div class="layui-upload">
-            <button type="button" class="layui-btn" id="uploadPic"><i class="layui-icon">&#xe67c;</i>图片上传</button>
-            <div class="layui-upload-list">
-                <ul id="layui-upload-box" class="layui-clear">
-                    @if(isset($member->avatar_url))
-                        <li><img src="{{ $member->avatar_url }}"/>
-                            <p>上传成功</p></li>
-                    @endif
-                </ul>
-                <input type="hidden" name="avatar_url" id="avatar_url" value="{{ $member->avatar_url??'' }}">
-            </div>
-        </div>
-    </div>
-</div>
+{{--<div class="layui-form-item">--}}
+{{--    <label for="" class="layui-form-label">头像</label>--}}
+{{--    <div class="layui-input-block">--}}
+{{--        <div class="layui-upload">--}}
+{{--            <button type="button" class="layui-btn" id="uploadPic"><i class="layui-icon">&#xe67c;</i>图片上传</button>--}}
+{{--            <div class="layui-upload-list">--}}
+{{--                <ul id="layui-upload-box" class="layui-clear">--}}
+{{--                    @if(isset($member->avatar_url))--}}
+{{--                        <li><img src="{{ $member->avatar_url }}"/>--}}
+{{--                            <p>上传成功</p></li>--}}
+{{--                    @endif--}}
+{{--                </ul>--}}
+{{--                <input type="hidden" name="avatar_url" id="avatar_url" value="{{ $member->avatar_url??'' }}">--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
 <div class="layui-form-item">
     <div class="layui-input-block">
         <button type="submit" class="layui-btn" lay-submit="" lay-filter="formDemo">确 认</button>
